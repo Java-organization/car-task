dfcf62fbeec8c72464ed3246dbd04033a722f463
sebnem <sebnem@localhost>
2022-06-07 22:40:08 +0400

    change from public of private

--- Main/src/main/Car.java
+++ Main/src/main/Car.java
@@ -1,9 +1,28 @@
 package main;
 
 public class Car implements CarInterface {
-	String name;
-	int speed;
-	int price;
+	private String name;
+	private	int speed;
+	private	int price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
 	@Override
 	public void carSpeed(String name, int speed) {
 		System.out.println("car name "+name+" car speed " +speed);
--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -2,20 +2,19 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		Car car = new Car();
-		car.name = "bmw";
-		car.speed = 400;
-		car.price = 2000;
-		car.carSpeed(car.name, car.speed);
-		car.carPrice(car.name, car.price);
-		Car car1 = new Car();
-		car1.name = "jeep";
-		car1.speed = 600;
-		car1.price = 10000;
-		car1.carSpeed(car1.name, car1.speed);
-		car1.carPrice(car1.name, car1.price);
+		car.setName("bmw");
+		car.setSpeed(400);
+		car.setPrice(20000); 
+		car.carSpeed(car.getName(), car.getSpeed());
+		car.carPrice(car.getName(), car.getPrice());
+		// Car car1 = new Car();
+		// car1.name = "jeep";
+		// car1.speed = 600;
+		// car1.price = 10000;
+		// car1.carSpeed(car1.name, car1.speed);
+		// car1.carPrice(car1.name, car1.price);
 	}
-	
 
 }
